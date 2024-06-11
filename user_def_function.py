aa4831128d4262aa361eeb7c0dc2430b4194b6b0
student-02-b1edbc847490 <kambletanaya18@gmail.com>
2024-06-11 00:05:59 -0700

    'userdef'

---
+++ user_def_function.py
@@ -0,0 +1,41 @@
+# Variable in user defined Function
+
+# Function with no argument no return value
+##def add():
+##    a=10
+##    b=20
+##    print(f"addition of {a} and {b} is:",a+b)
+##add()
+
+
+# Function with argument no return value
+
+##def add(a,b):
+##    print(f"addition of {a} and {b} is:",a+b)
+##a=10
+##b=20
+##add(a,b)
+
+
+# Function with no argument but return value
+##def add():
+##    a=10
+##    b=20
+##    return a+b
+##    #return f"addition of {a} and {b} is:",a+b
+##sum=add()
+##print(sum)
+
+
+# Function with argument and return value
+def add(a,b):
+    return a+b
+    print("hii")#after return no executable
+a=8
+b=9
+sum=add(a,b)
+print(sum)
+
+
+
+
